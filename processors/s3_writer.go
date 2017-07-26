db947c46745863ca35f5f2781fca0f94025f4e47
Greggory Rothmeier <greggroth@gmail.com>
2017-07-26 15:25:20 -0700

    Use improved s3writer

--- processors/s3_writer.go
+++ processors/s3_writer.go
@@ -1,47 +1,67 @@
 package processors
 
 import (
+	"compress/gzip"
+	"io/ioutil"
+	"os"
+	"strings"
+
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/bluebottlecoffee/ratchet/data"
-	"github.com/bluebottlecoffee/ratchet/util"
 )
 
-// S3Writer sends data upstream to S3. By default, we will not compress data before sending it.
-// Set the `Compress` flag to true to use gzip compression before storing in S3 (if this flag is
-// set to true, ".gz" will automatically be appended to the key name specified).
-//
-// By default, we will separate each iteration of data sent to `ProcessData` with a new line
-// when we piece back together to send to S3. Change the `LineSeparator` attribute to change
-// this behavior.
 type S3Writer struct {
-	data          []string
-	Compress      bool
-	LineSeparator string
-	config        *aws.Config
-	bucket        string
-	key           string
+	bucket  string
+	key     string
+	tmpfile *os.File
+	gz      *gzip.Writer
+	svc     *s3.S3
 }
 
-// NewS3Writer instaniates a new S3Writer
-func NewS3Writer(awsID, awsSecret, awsRegion, bucket, key string) *S3Writer {
-	w := S3Writer{bucket: bucket, key: key, LineSeparator: "\n", Compress: false}
+func NewS3Writer(awsRegion, bucket, key string) *S3Writer {
+	w := S3Writer{bucket: bucket, key: key}
 
-	creds := credentials.NewStaticCredentials(awsID, awsSecret, "")
-	// .WithLogLevel(aws.LogDebugWithRequestRetries | aws.LogDebugWithRequestErrors)
-	w.config = aws.NewConfig().WithRegion(awsRegion).WithDisableSSL(true).WithCredentials(creds)
+	tmpfile, err := ioutil.TempFile(os.TempDir(), strings.Replace(key, string(os.PathSeparator), "-", 3))
+
+	if err != nil {
+		panic(err)
+	}
+	w.tmpfile = tmpfile
+	w.gz = gzip.NewWriter(tmpfile)
+
+	mySession := session.New(&aws.Config{Region: aws.String(awsRegion)})
+	w.svc = s3.New(mySession)
 
 	return &w
 }
 
-// ProcessData enqueues all received data
 func (w *S3Writer) ProcessData(d data.JSON, outputChan chan data.JSON, killChan chan error) {
-	w.data = append(w.data, string(d))
+	if _, err := w.gz.Write(d); err != nil {
+		killChan <- err
+	}
 }
 
-// Finish writes all enqueued data to S3, defering to util.WriteS3Object
 func (w *S3Writer) Finish(outputChan chan data.JSON, killChan chan error) {
-	util.WriteS3Object(w.data, w.config, w.bucket, w.key, w.LineSeparator, w.Compress)
+	var err error
+
+	if err = w.gz.Close(); err != nil {
+		killChan <- err
+	}
+
+	params := &s3.PutObjectInput{
+		Bucket: aws.String(w.bucket),
+		Key:    aws.String(w.key),
+		Body:   w.tmpfile,
+	}
+	_, err = w.svc.PutObject(params)
+
+	if err != nil {
+		killChan <- err
+	}
+
+	os.Remove(w.tmpfile.Name())
 }
 
 func (w *S3Writer) String() string {
